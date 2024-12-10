8308449d79784da9a267cc69ae8f4a5633ef3fcb
shitalmarathe <shitalmarathe57@gmail.com>
2024-12-10 13:25:08 +0000

    add components and alias

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import Aside from "@components/Aside";
 
 function Home(){
     return <div>HOME</div>
+    return(
+        <div>
+            <Header />
+            <Form />
+            <Aside />
+        </div>
+    );
 }
 
 export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Home from "../pages/Home";
-import "./global.css";
+import Home from "@/pages/Home";
+import "@/global.css";
 
 const appEl = document.querySelector("#app");
 const root = createRoot(appEl);
